20bbc1f5d7fb65388c6281040bb702519df8cb37
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-13 10:15:10 -0400

    Printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
