868186b17031989e4e39e8b30f0973f9d04f1244
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-16 12:37:20 -0400

    print all combinations of single digits

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single - digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
