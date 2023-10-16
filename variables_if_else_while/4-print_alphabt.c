95c7658d55b5d3d9ff80da12bc660c151185ebcb
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-16 10:59:02 -0400

    prints the alphabet except q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h
+/**
+ * main - print the alphabet except for q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++a)
+	{
+		if( a != 'q' || a != 'e')
+		{
+			putchar(a);
+		}
+	}
+	return 0;
+}
+		
