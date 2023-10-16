3989813f5f933930ad42efb7034d33a05f6ef603
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-16 11:29:35 -0400

    printing alphabet in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - print the alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; --a)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
