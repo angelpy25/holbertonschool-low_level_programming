9e09e48421e7ec937f353ace37757b05067c85db
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-16 10:23:16 -0400

    if else with module

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ int main(void)
 	/* your code goes there */
 	if (last_digit == 0)
 	{
-		 printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
-	else if (last_digit < 5 )
+	else if (last_digit < 5)
 	{
-		 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
 	else
 	{
