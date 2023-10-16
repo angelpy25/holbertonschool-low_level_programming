d5b3bfc232c0d24e3f0a8818e9706c3d6b115b68
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-16 10:48:00 -0400

    prints the alphabet in lowercase and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase and uppercase
+ *Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'Z'; ++a)
+	{
+		putchar(a);
+	}
+	return (0);
+}
