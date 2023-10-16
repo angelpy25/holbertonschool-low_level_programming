3c502598b957c71aa43b63df23c4563738a6051c
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-16 13:30:06 -0400

    print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print the alphabet
+ * Return: 0
+ *
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a < 'z'; ++a)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
