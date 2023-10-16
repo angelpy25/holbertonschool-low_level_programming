30e156815dfa27247106370cb0eaada0a7616874
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-16 11:15:10 -0400

    prints all numbers in base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers in  base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		printf(%d,i);
+	}
+	putchar('\n');
+	return (0);
