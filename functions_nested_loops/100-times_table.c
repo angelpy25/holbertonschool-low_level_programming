f4adb4c75d0f24f878aa81b427132097b4c17c25
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-18 14:32:55 -0400

    prints times table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -20,11 +20,6 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				m = i * j;
-				if ( j == 0)
-				{
-					_putchar('0');
-					continue;
-				}
 				if ( m >= 10)
 				{
 					printf(", %d", m);
