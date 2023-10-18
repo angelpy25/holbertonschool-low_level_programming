3b7d008f136d338fa167695f6adfae692d9d8058
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-18 13:49:09 -0400

    prints all natural numbers up to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - print all natural numbers up to 98
@@ -6,50 +7,19 @@
  */
 void print_to_98(int n)
 {
-	int abs, fnmbr, lnmbr;
-
-	for (; n < 99; n++)
+	if (n > 98)
 	{
-		if (n < 0)
+		for (; n > 98; n--)
 		{
-			if (n < -9)
-			{
-				abs = n * -1;
-				fnmbr = abs / 10;
-				lnmbr = abs % 10;
-				_putchar('-');
-				_putchar('0' + fnmbr);
-				_putchar('0' + lnmbr);
-				_putchar(',');
-				_putchar(' ');
-			}
-			abs = n * -1;
-			_putchar('-');
-			_putchar('0' + abs);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
-		else if (n >= 0 && n <=9)
+	}
+	if (n < 98)
+	{
+		for (; n < 98; n++)
 		{
-			_putchar('0' + n);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
-		else if (n >= 10 && n <= 97)
-		{
-			fnmbr = n / 10;
-			lnmbr = n % 10;
-			_putchar('0' + fnmbr);
-			_putchar('0' + lnmbr);
-			_putchar(',');
-			_putchar(' ');
-		}
-		else
-		{
-			fnmbr = n / 10;
-			lnmbr = n % 10;
-			_putchar('0' + fnmbr);
-			_putchar('0' + lnmbr);
-		}
-	}	
+	}
+	printf("%d, \n",n);
 }
