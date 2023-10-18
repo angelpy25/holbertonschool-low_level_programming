a38cd19eab2e1852b886d6ee41893b7241d04ca4
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-18 12:32:07 -0400

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: return the result of the addition of the 2 parameters
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
