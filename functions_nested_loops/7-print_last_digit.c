87d933b5dc315e7c05d70899e567c981a0c7b2d9
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-18 09:30:15 -0400

    prints the last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @pld: number that will be examine
+ * Return: return the last number of pld
+ */
+int print_last_digit(int pld)
+{
+	int last = pld % 10;
+
+	_putchar('0' + last);
+	return (last);
+}
