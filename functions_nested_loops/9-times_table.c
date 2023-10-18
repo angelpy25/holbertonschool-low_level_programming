c29936da8200b62899d49013f5c8d0c8a250b00c
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-18 10:53:22 -0400

    print the 9 table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,16 +5,28 @@
  */
 void times_table(void)
 {
-	int i, j, m;
+	int i, j, m, mlast, mfirst;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			m = i * j;
+			if ( m >= 10)
+			{
+				mlast = m % 10;
+				mfirst = m /10;
+				_putchar('0' + mfirst);
+				_putchar('0' + mlast);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
 			_putchar('0' + m);
 			_putchar(',');
 			_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
