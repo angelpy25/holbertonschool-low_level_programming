bc2ca2dbe0fb08023651387f0db93f3b0d084802
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-18 08:56:36 -0400

    check if it is positive, negative or zero

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * print_sign - given a number prints if it is positive, negative or zero
+ * @n: the number given
+ * Return: it varies the result it returns what the result is
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
