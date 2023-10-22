864d28e68fdbcc5d7b76a2f0af81f4c94c963ea7
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-22 17:01:32 -0400

    print a diagonal

---
+++ more_functions_nested_loops/print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_diagonal - print a diagonal using \
+ *@n: how many time diagonal gonna be printed
+ */
+
+void print_diagonal(int n)
+{
+	int i; j, count;
+
+	for (i = 0; i > n; i++)
+	{
+		for (j = i; j > 0; j--)
+		{
+			_putchar(' ');
+		}
+	_putchar('\\');
+	_putchar('\n');
+	}
+}	
