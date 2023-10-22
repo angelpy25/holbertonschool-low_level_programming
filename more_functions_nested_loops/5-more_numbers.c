d28772c77b78240a370446ffdafa688299f0481c
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-22 15:51:49 -0400

    print 10 times from 1 to 14

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,13 @@
  */
 void more_numbers(void)
 {
-	int i, j, x;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 14; j++)
 		{
-			if (b > 9)
+			if (j > 9)
 			_putchar('0' + j / 10);
 			_putchar('0' + j % 10);
 		}
