e5e8b3a867d91c3a393ebde018537ec4934564e6
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-22 12:19:38 -0400

    multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "mai.h"
+
+/**
+ * mul - multiply to parameters given
+ * @a: first number
+ * @b: second number
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
