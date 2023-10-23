a602987e60054369ffc6216a72ac7ad0af26171e
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-22 22:06:46 -0400

    print triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_triangle - Print an rectangle triangle
+ *@size: Size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, count;
+
+	count = size -1;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < count; j++)
+			_putchar(' ');
+		for (; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+		count -= 1;
+	}
+}
