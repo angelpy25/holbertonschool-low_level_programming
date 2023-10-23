b645d740f5a8665911babfdc3a19f87819e16960
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-22 21:43:45 -0400

    print from 1 to 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i == 100)
-			printf("FizzBuzz")
+			printf("FizzBuzz");
 		else if ((i % 3) == 0 && (i % 5) == 0)
 		{
 			printf("FizzBuzz ");
