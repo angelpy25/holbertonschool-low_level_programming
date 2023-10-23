17aabdd26f9c8ac926c167fbae8a888d24550b76
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-23 11:36:16 -0400

    string length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * strlen - it count the string length
+ * @s: string given
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while(s[count] != '\0')
+		count++;
+
+	return (count);
+}
