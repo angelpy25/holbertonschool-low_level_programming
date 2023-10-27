7d8ef55707d6d7953f558e3b6f72da1131dcec7d
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-27 13:55:01 -0400

    Capitalize every string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,14 +12,7 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] >= 32 && str[i] <= 47) || (str[i] >= 58 && str[i] <= 63))
-		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-				str[i + 1] = str[i + 1] - 32;
-			else
-				continue;
-		}
-		if (str[i] == '\n' || str[i] == '\t')
+		if ((str[i] >= 1 && str[i] <= 47) || (str[i] >= 58 && str[i] <= 63))
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 				str[i + 1] = str[i + 1] - 32;
