24ceeaee82ab06ba2214b4361dbc4fd00967eb2f
angelpy25 <angeltoy02.ar@gmail.com>
2023-10-27 11:07:24 -0400

    changes lower to upper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lower to upper
+ * @str: string given
+ * Return: the string in upper
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
