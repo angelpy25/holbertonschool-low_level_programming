d481ee9dafbc807564e8491b07f4b058aea12d5e
angelpy25 <angeltoy02.ar@gmail.com>
2023-11-03 09:00:04 -0400

    prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * pmf - prime number finder
+ * @a: number given
+ * @b: number of iterations
+ * Return: if it is a prime
+ */
+
+int pmf(int a, int b)
+{
+	if (a <= 1 || a % b == 0)
+		return (0);
+	else if (a == b)
+		return (1);
+	else if (a > b)
+		return (pmf(a, b + 1));
+	return (1);
+/**
+ * is_prime_number - return the prime number
+ * @n: number given
+ * Return: 0 or 1 if it is a prime number
+ */
+int is_prime_number(int n)
+{
+	return (pmf(n, 2));
+}
