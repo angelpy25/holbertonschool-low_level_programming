1667443b86f19551b06bd772ec1d34bc03fb9125
angelpy25 <angeltoy02.ar@gmail.com>
2023-11-03 09:57:26 -0400

    count arguments

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int _atribute_((_unused_)) argc, char *argv[])
+int main(int __atribute__((unused)) argc, char *argv[])
 {
 	printf("%s\n", *argv[0]);
 	return (0);
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int main(int argc, char __attribute__((unused)) **argv)
 {
 	int count = 0;
 
