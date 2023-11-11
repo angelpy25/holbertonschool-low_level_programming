536c72e0a17f4f14d12729c051497b2ae0ce579b
angelpy25 <angeltoy02.ar@gmail.com>
2023-11-10 22:32:41 -0400

    duplicate a string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strdup - duplicate a string
+ * @str: string given
+ * Return: NULL or the direction of the duplicate string
+ */
+
+char *strdup(char *str)
+{
+	char *c;
+	int i, j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	c = malloc(i * sizeof(*c) + 1);
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	{
+		c[j] = str[j];
+	}
+	c[j] = '\0';
+
+	return (c);
+}
