e9748e74c9c60ee2f634722b4692b1a617b529fa
angelpy25 <angeltoy02.ar@gmail.com>
2023-11-10 22:19:42 -0400

    create a dynamic array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create a string
+ * @size: size of the string given
+ * @c: string given
+ * Return: the string or NUll
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	p = malloc(size * sizeof(*p));
+
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
+	return (a);
+}
